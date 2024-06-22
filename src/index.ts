afada5ed2d1e62c24905bd2a4c1b3760d89db2b1
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-22 15:12:21 +0800

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import type {  } from 'unocss'
+
+export const presetHaixee = () => {}
---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import { presetUno } from 'unocss'
+import { presetHaixee } from './src'
+import { presetTypography } from 'unocss'
+import unocss from 'unocss/vite'
+
+export default defineConfig(({ mode }) => ({
+  base: '',
+  plugins: [
+    dts(),
+    unocss({
+      presets: [
+        presetUno(),
+        presetTypography({
+          cssExtend: {
+            code: {
+              color: '#8b5cf6',
+            },
+            'a:hover': {
+              color: '#f43f5e',
+              'text-decoration': 'none',
+            },
+            'a:visited': {
+              color: '#14b8a6',
+            },
+          },
+        }),
+        presetHaixee(),
+      ],
+    }),
+  ],
+  build: {
+    minify: mode === 'demo',
+    lib:
+      mode === 'demo'
+        ? false
+        : {
+            entry: './src/index.ts',
+            formats: ['es'],
+            fileName: () => 'index.js',
+          },
+    rollupOptions: {
+      external: ['unocss', /^@unocss/],
+    },
+  },
+}))
